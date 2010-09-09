1becbc4666f87c01f0b9ae008acca8e167fd3825
Anthony Burns <anthony@dirtyalpaca.com>
2010-09-09 13:34:26 -0500

    Commit initial spike

--- lib/browser_body_tag_helper.rb
+++ lib/browser_body_tag_helper.rb
@@ -1,15 +1,22 @@
 module BrowserBodyTagHelper
 
   def browser_body_tag(html_options = {}, &block)
-    if block_given?
-      
-    else
+    ie_tag_conditional("lt IE 7", tag(:body, merge_class("ie6", options))
+    (7..9).each { |n| ie_tag_conditional("IE #{n}", tag(:body, merge_class("ie#{n}")) }
+    raw '<!--[if (gt IE 9) | !(IE)]><!-->') + tag(:body, attrs) + raw('<!--<![endif]-->'
 
+    if block_given?
+      yield
+      raw('</body>'
     end
   end
 
-  def browser_wrap_conditional(condition, tag)
-    raw("<!--[if #{condition}]#{tag}<![endif]-->")
+  def ie_tag_conditional(condition, tag)
+    raw "<!--[if #{condition}]>" + tag + "<![endif]-->"
+  end
+
+  def merge_class(class_name, attrs = {})
+    attrs.merge(:class => [attrs[:class], class_name].join(' '))
   end
   
 end
