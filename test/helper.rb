e8d2d19aeaae1db7cb531842040dee42fa62c35a
Anthony Burns <anthony@dirtyalpaca.com>
2010-09-09 13:56:06 -0500

    ActionView test scaffolding

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,11 @@
+require 'rubygems'
 require 'test/unit'
+require 'active_support'
+require 'active_support/core_ext'
+require 'action_view'
+require 'action_controller'
+require 'action_controller/test_case'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'browser_body_tag'
-
-class Test::Unit::TestCase
-end
+require 'browser_body_tag_helper'
