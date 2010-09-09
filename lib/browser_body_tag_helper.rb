445acf7808b7fec67fd382224d6623159ca4448d
Anthony Burns <anthony@dirtyalpaca.com>
2010-09-09 13:55:57 -0500

    Fix syntax errors

--- lib/browser_body_tag_helper.rb
+++ lib/browser_body_tag_helper.rb
@@ -1,13 +1,13 @@
 module BrowserBodyTagHelper
 
   def browser_body_tag(html_options = {}, &block)
-    ie_tag_conditional("lt IE 7", tag(:body, merge_class("ie6", options))
-    (7..9).each { |n| ie_tag_conditional("IE #{n}", tag(:body, merge_class("ie#{n}")) }
-    raw '<!--[if (gt IE 9) | !(IE)]><!-->') + tag(:body, attrs) + raw('<!--<![endif]-->'
+    ie_tag_conditional("lt IE 7", tag(:body, merge_class("ie6", options)))
+    (7..9).each { |n| ie_tag_conditional("IE #{n}", tag(:body, merge_class("ie#{n}"))) }
+    raw('<!--[if (gt IE 9) | !(IE)]><!-->') + tag(:body, attrs) + raw('<!--<![endif]-->')
 
     if block_given?
       yield
-      raw('</body>'
+      raw('</body>')
     end
   end
 
