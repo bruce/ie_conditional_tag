2750156a6492fc79ff7594270f7af01ea0725a7f
Bruce Williams <bruce@codefluency.com>
2010-09-09 11:03:39 -0700

    Initial commit to browser_body_tag.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'browser_body_tag'
+
+class Test::Unit::TestCase
+end
